994d3406df566a8f804a3be1142ea5a82dbd93dc
Nataly <105498408+natysych@users.noreply.github.com>
2025-11-27 22:03:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 DOMAIN = os.getenv("DOMAIN")
 WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/webhook")
-WEBHOOK_URL = DOMAIN + WEBHOOK_PATH
+WEBHOOK_URL = f"{DOMAIN}{WEBHOOK_PATH}"
 
-# 👇 ОБОВ'ЯЗКОВО ВСТАВЛЯЄМО ОТАК
 WEBAPP_HOST = "0.0.0.0"
 WEBAPP_PORT = int(os.getenv("PORT", 8000))
