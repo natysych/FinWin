9635f144b9e2fe3c7d60ee83b471e53d094a842c
Nataly <105498408+natysych@users.noreply.github.com>
2025-11-27 19:03:48 +0200

    Create start.py

---
+++ routers/routers/start.py
@@ -0,0 +1,23 @@
+from aiogram import Router, types
+from keyboards.start_kb import start_keyboard, continue_keyboard
+
+router = Router()
+
+@router.message(commands=["start"])
+async def start_cmd(message: types.Message):
+    await message.answer(
+        "🎉 Вітаємо! Ви підписалися на бот FinanceForTeens!\n\n"
+        "Це курс з фінансової грамотності...",
+        reply_markup=start_keyboard()
+    )
+
+
+@router.callback_query(lambda c: c.data == "start_yes")
+async def start_yes(callback: types.CallbackQuery):
+    await callback.message.edit_text(
+        "Інформація на курс розрахована на 14–19 років 📚\n..."
+    )
+    await callback.message.answer(
+        "Продовжимо навчання?",
+        reply_markup=continue_keyboard()
+    )
