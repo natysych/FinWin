56f3a23023920aa819993f12fbaa4a6942a1ad80
Nataly <105498408+natysych@users.noreply.github.com>
2025-11-29 14:29:00 +0200

    Update offer.py

--- routers/offer.py
+++ routers/offer.py
@@ -2,10 +2,10 @@ from aiogram import Router, types
 
 router = Router()
 
+
 @router.callback_query(lambda c: c.data == "offer")
 async def send_offer(callback: types.CallbackQuery):
-    with open("offer.pdf", "rb") as file:
-        await callback.message.answer_document(
-            document=file,
-            caption="📄 Публічна оферта"
-        )
+    await callback.message.answer_document(
+        document=types.FSInputFile("offer.pdf"),
+        caption="📄 Публічна оферта"
+    )
