a5fe2299d5abb79966161e7dc50d26aabeb815d0
Nataly <105498408+natysych@users.noreply.github.com>
2025-11-30 21:41:09 +0200

    Update start_kb.py

--- keyboards/start_kb.py
+++ keyboards/start_kb.py
@@ -1,17 +1,19 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+
 def start_keyboard():
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text="✨ Так, хочу далі!", callback_data="start_yes")],
-            [InlineKeyboardButton(text="❌ Ні, відписатись", callback_data="start_no")]
+            [InlineKeyboardButton(text="❌ Ні, відписатись", callback_data="start_no")],
         ]
     )
+
 
 def continue_keyboard():
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text="👉 Так, продовжимо!", callback_data="cont_yes")],
-            [InlineKeyboardButton(text="❌ Відписатись", callback_data="start_no")]
+            [InlineKeyboardButton(text="❌ Відписатись", callback_data="start_no")],
         ]
     )
