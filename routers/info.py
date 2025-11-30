ceaa0e4fc529bd4ba8f55d139a1ac3eef4acfa49
Nataly <105498408+natysych@users.noreply.github.com>
2025-11-30 16:48:38 +0200

    Update info.py

--- routers/info.py
+++ routers/info.py
@@ -3,16 +3,15 @@ from aiogram.filters import Command
 
 router = Router()
 
-
 @router.message(Command("info"))
 async def info_cmd(message: types.Message):
     text = (
-        "📍  💼 *FinanceForTeens* — освітній проєкт компанії *Finterra*.\n"
+        "💼 *FinanceForTeens* — освітній проєкт компанії *Finterra*.\n\n"
         "👤 ФОП *Сич Наталія Вікторівна*\n"
-        "ФОП 3-тя група (без ПДВ)\n\n"
+        "📌 ФОП 3-тя група (без ПДВ)\n\n"
         "📞 +380672899005\n"
         "📧 finterra.com.ua@gmail.com\n\n"
-        "📄 Публічна оферта доступна в головному меню в розділі «Оплата»"
+        "📄 Публічна оферта доступна в меню «Оплата»"
     )
 
     await message.answer(text, parse_mode="Markdown")
